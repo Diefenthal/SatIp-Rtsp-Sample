fe02539c02daaee3332af30dfb146e0477f6d665
Kay Diefenthal <KayDiefenthal@hotmail.de>
2016-01-28 18:08:23 +0100

    update (C) header
    update (C) header

--- Rtsp/RtspStatusCode.cs
+++ Rtsp/RtspStatusCode.cs
@@ -1,5 +1,5 @@
 ﻿/*  
-    Copyright (C) <2007-2015>  <Kay Diefenthal>
+    Copyright (C) <2007-2016>  <Kay Diefenthal>
 
     SatIp.RtspSample is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
